3f0e468ba09d94bfca519478a275799332de33e3
keziarkts <113933860+keziarkts@users.noreply.github.com>
2023-08-27 14:12:43 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function goToTanyaPage() {
+    window.location.href = "index.html";
+}
+
+function goToJohnPage() {
+    window.location.href = "john-page.html";
+}
+
+// For transition between pages:
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 700)
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1
+})
+
